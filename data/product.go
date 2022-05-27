9dfa5c4442db03330c2c2869552e6f13a9c758e7
RonakR <rabadiyaronak@gmail.com>
2022-05-27 20:47:37 +0530

    add product-api complete

--- data/product.go
+++ data/product.go
@@ -23,8 +23,22 @@ func (p *Products) ToJson(w io.Writer) error {
 	return json.NewEncoder(w).Encode(p)
 }
 
+func (p *Product) FromJSON(r io.Reader) error {
+	return json.NewDecoder(r).Decode(p)
+}
+
 func GetProducts() Products {
 	return productList
+}
+
+func AddProduct(p *Product) {
+	p.ID = getNextId()
+	productList = append(productList, p)
+}
+
+func getNextId() int {
+	lp := productList[len(productList)-1]
+	return lp.ID + 1
 }
 
 var productList = []*Product{
--- handler/product.go
+++ handler/product.go
@@ -21,6 +21,11 @@ func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		return
 	}
 
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
+
 	//default catch for not supported method
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 
@@ -36,4 +41,19 @@ func (p *Product) getProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+
+}
+
+func (p *Product) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+
+	product := &data.Product{}
+
+	err := product.FromJSON(r.Body)
+
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal request", http.StatusBadRequest)
+	}
+
+	data.AddProduct(product)
 }
