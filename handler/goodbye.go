5999cc3a2ea56d69d94f29213b42adb2ad07ec8b
RonakR <rabadiyaronak@gmail.com>
2022-05-27 19:01:12 +0530

    hello world with sample server

---
+++ handler/goodbye.go
@@ -0,0 +1,18 @@
+package handler
+
+import (
+	"log"
+	"net/http"
+)
+
+type Goodbye struct {
+	l *log.Logger
+}
+
+func NewGoodbye(l *log.Logger) *Goodbye {
+	return &Goodbye{l}
+}
+
+func (g *Goodbye) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	rw.Write([]byte("Byeee\n"))
+}
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"essential-practices/project/working/handler"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"time"
+)
+
+func main() {
+	l := log.New(os.Stdout, "product-api", log.LstdFlags)
+	hh := handler.NewHello(l)
+	gh := handler.NewGoodbye(l)
+
+	sm := http.NewServeMux()
+	sm.Handle("/", hh)
+	sm.Handle("/goodbye", gh)
+
+	s := &http.Server{
+		Addr:         ":9090",
+		Handler:      sm,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+		IdleTimeout:  120 * time.Second,
+	}
+
+	go func() {
+		err := s.ListenAndServe()
+		if err != nil {
+			l.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+
+	l.Println("Received terminate, graceful shutdown", sig)
+
+	tc, _ := context.WithTimeout(context.Background(), 30*time.Second)
+	s.Shutdown(tc)
+}
