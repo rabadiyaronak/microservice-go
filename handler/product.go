0d3cff914e3f39e2eff9d74c8a2f231dc6f9efbf
RonakR <rabadiyaronak@gmail.com>
2022-05-30 14:52:58 +0530

    remove commented code

--- handler/product.go
+++ handler/product.go
@@ -18,54 +18,6 @@ func NewProduct(l *log.Logger) *Product {
 	return &Product{l}
 }
 
-// func (p *Product) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-
-// 	if r.Method == http.MethodGet {
-// 		p.getProducts(rw, r)
-// 		return
-// 	}
-
-// 	if r.Method == http.MethodPost {
-// 		p.addProduct(rw, r)
-// 		return
-// 	}
-// 	if r.Method == http.MethodPut {
-// 		p.l.Println("PUT", r.URL.Path)
-
-// 		regexpIdFinder := regexp.MustCompile(`/([0-9]+)`)
-// 		g := regexpIdFinder.FindAllStringSubmatch(r.URL.Path, -1)
-
-// 		if len(g) != 1 {
-// 			p.l.Println("Invalid URI more than one id")
-// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		if len(g[0]) != 2 {
-// 			p.l.Println("Invalid URI capture group should be two")
-// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		idString := g[0][1]
-// 		id, err := strconv.Atoi(idString)
-// 		if err != nil {
-// 			p.l.Println("Invalid URI unable to convert id to number")
-// 			http.Error(rw, "Invalid URI", http.StatusBadRequest)
-// 			return
-// 		}
-
-// 		p.l.Println("extracted id ", id)
-
-// 		p.updateProduct(id, rw, r)
-
-// 	}
-
-// 	//default catch for not supported method
-// 	rw.WriteHeader(http.StatusMethodNotAllowed)
-
-// }
-
 func (p *Product) GetProducts(rw http.ResponseWriter, r *http.Request) {
 	p.l.Println("Handle GET Products")
 
